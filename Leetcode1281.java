103f0f9edd92392e947d30ff0e9fd97f6ae960e1
Chethanravindra09 <chethan.cravindra@gmail.com>
2024-01-17 18:27:10 +0530

    Leetcode-1281 solution added

--- Leetcode1281.java
+++ Leetcode1281.java
@@ -1,2 +1,30 @@
-public class Leetcode01 {
+import java.util.Scanner;
+
+class Solution {
+    public int subtractProductAndSum(int n) {
+
+        int sum = 0;
+        int prod = 1;
+        while (n > 0) {
+            int rem = n % 10;
+            sum += rem;
+            prod *= rem;
+            n = n / 10;
+        }
+
+        return (prod - sum);
+    }
+}
+
+public class Leetcode1281 {
+
+    public static void main(String[] args){
+        Solution s1= new Solution();
+        Scanner sc= new Scanner(System.in);
+        int n ;
+        System.out.println("Enter the value of n");
+        n=sc.nextInt();
+        int output =s1.subtractProductAndSum(n);
+        System.out.println(output);
+    }
 }
